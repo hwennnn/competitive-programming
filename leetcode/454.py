82560ef548370aa5c4dc6cabf0a2e05236e4086d
hwen <whman63@gmail.com>
2020-09-02 21:44:34 +0800

    2/9/2020

---
+++ leetcode/454.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/4sum-ii/
+
+class Solution:
+    def fourSumCount(self, A: List[int], B: List[int], C: List[int], D: List[int]) -> int:
+        AB = {}
+        res = 0
+        
+        for a in A:
+            for b in B:
+                AB[a+b] = AB.get(a+b,0) + 1
+        
+        for c in C:
+            for d in D:
+                res += AB.get(-c-d,0)
+        
+        return res
