360c09a1367a50f73a41f31ac22b74cb06dc957d
hwen <whman63@gmail.com>
2020-09-22 20:25:06 +0800

    revisited for weekly 207 qn3

--- leetcode/Contest/Weekly/207/1594.py
+++ leetcode/Contest/Weekly/207/1594.py
@@ -22,3 +22,34 @@ class Solution:
                 Min[i][j] = min(Max[i-1][j] * A[i][j], Min[i-1][j] * A[i][j], Max[i][j-1] * A[i][j], Min[i][j-1] * A[i][j])
 
         return Max[-1][-1] % int(1e9 + 7) if Max[-1][-1] >= 0 else -1
+
+# revisited solutin
+class Solution:
+    def maxProductPath(self, A: List[List[int]]) -> int:
+        M = 1000000007
+        row, col = len(A), len(A[0])
+        
+        mp = [[0] * col for _ in range(row)] 
+        mn = [[0] * col for _ in range(row)] 
+        mp[0][0] = mn[0][0] = A[0][0]
+        
+        for i in range(1,col):
+            mp[0][i] = A[0][i] * mp[0][i-1]
+            mn[0][i] = A[0][i] * mp[0][i-1]
+        
+        for i in range(1,row):
+            mp[i][0] = A[i][0] * mp[i-1][0]
+            mn[i][0] = A[i][0] * mp[i-1][0]
+            
+        for i in range(1,row):
+            for j in range(1,col):
+                if A[i][j] > 0:
+                    mp[i][j] = max(mp[i-1][j], mp[i][j-1]) * A[i][j]
+                    mn[i][j] = min(mn[i-1][j], mn[i][j-1]) * A[i][j]
+                else:
+                    mp[i][j] = min(mn[i-1][j], mn[i][j-1]) * A[i][j]
+                    mn[i][j] = max(mp[i-1][j], mp[i][j-1]) * A[i][j]
+        
+        res = mp[row-1][col-1]
+        
+        return res%M if res > -1 else -1
