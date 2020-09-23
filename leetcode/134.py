d0c08c9e127908bd93708aeb2d2d496d40eed785
hwen <whman63@gmail.com>
2020-09-23 20:14:43 +0800

    Wed 23 Sep 2020 20:14:43 +08

---
+++ leetcode/134.py
@@ -0,0 +1,16 @@
+# 134. Gas Station
+# https://leetcode.com/problems/gas-station/
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        totalGas = totalCost = res = cur = 0
+        
+        for i, (g,c) in enumerate(zip(gas,cost)):
+            totalGas += g
+            totalCost += c
+            cur += (g-c)
+            if cur < 0:
+                cur = 0
+                res = i+1
+        
+        return -1 if totalGas < totalCost else res
