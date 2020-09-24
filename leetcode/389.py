8420b4282b70925b6d75661fe7f0dcca27bee6e2
hwen <whman63@gmail.com>
2020-09-24 18:50:09 +0800

    Thu 24 Sep 2020 18:50:09 +08

---
+++ leetcode/389.py
@@ -0,0 +1,14 @@
+# 389. Find the Difference
+# https://leetcode.com/problems/find-the-difference/
+
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        res = 0
+        
+        for c in t:
+            res += ord(c)
+        
+        for c in s:
+            res -= ord(c)
+            
+        return chr(res)
