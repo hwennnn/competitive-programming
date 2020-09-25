632d4cb500ab6cbc5988a2b510e662a95749ad07
hwen <whman63@gmail.com>
2020-09-25 20:09:34 +0800

    Fri 25 Sep 2020 20:09:34 +08

---
+++ leetcode/179.py
@@ -0,0 +1,11 @@
+# 179. Largest Number
+# https://leetcode.com/problems/largest-number/
+
+import functools 
+
+class Solution:
+    def largestNumber(self, nums):
+        compare = lambda a, b: 1 if a+b > b+a else -1 if a+b < b+a else 0
+        _nums = list(map(str, nums))
+        _nums.sort(key=functools.cmp_to_key(compare), reverse=True)
+        return str(int(''.join(_nums)))
