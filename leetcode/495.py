67f08698e4b05c8d41574de8c08006e8fe47d374
hwen <whman63@gmail.com>
2020-09-26 16:04:14 +0800

    teemo attacking

---
+++ leetcode/495.py
@@ -0,0 +1,19 @@
+# 495. Teemo Attacking
+# https://leetcode.com/problems/teemo-attacking/
+
+class Solution(object):
+    def findPoisonedDuration(self, ts, d):
+        n = len(ts)
+        if n == 0: return 0
+        
+        res = d
+        
+        for i in range(1, n):
+            if ts[i] - ts[i-1] > d:
+                res += d
+            else:
+                res += ts[i] - ts[i-1]
+        
+        return res
+            
+        
