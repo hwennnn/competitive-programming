26a7b595de48a8ce96c975ddccb1003cd756f48a
hwen <whman63@gmail.com>
2020-09-29 19:41:13 +0800

    word break

---
+++ leetcode/139.py
@@ -0,0 +1,18 @@
+# 139. Word Break
+# https://leetcode.com/problems/word-break/
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        
+        n = len(s)
+        check = [False] * (n+1)
+        check[0] = True
+        
+        for i in range(n):
+            if check[i]:
+                for j in range(i, n):
+                    if s[i:j+1] in wordDict:
+                        check[j+1] = True
+        
+        return check[-1]
+        
