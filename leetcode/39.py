7ddf0bdf18425073a9b0ec4eb6a77ae115b8b98c
hwen <whman63@gmail.com>
2020-10-02 19:05:16 +0800

    Fri  2 Oct 2020 19:05:16 +08

---
+++ leetcode/39.py
@@ -0,0 +1,20 @@
+# 39. Combination Sum
+# https://leetcode.com/problems/combination-sum/
+
+class Solution:
+    def combinationSum(self, candidates, target):
+
+        res = []
+        candidates.sort()
+        
+        def dfs(target, index, path):
+            if target < 0:
+                return  # backtracking
+            if target == 0:
+                res.append(path)
+                return 
+            for i in range(index, len(candidates)):
+                dfs(target-candidates[i], i, path+[candidates[i]])
+        
+        dfs(target, 0, [])
+        return res
