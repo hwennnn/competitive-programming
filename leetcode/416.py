7fbdcacf1a6d0c83ee257f88eaa7798d0bb9bc34
hwen <whman63@gmail.com>
2020-10-02 21:37:25 +0800

    bitset

---
+++ leetcode/416.py
@@ -0,0 +1,14 @@
+# 416. Partition Equal Subset Sum
+# https://leetcode.com/problems/partition-equal-subset-sum/
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+
+        total = sum(nums)
+        if total%2: return False # odd sum cannot be partitioned equally
+        
+        bits = 1
+        for num in nums:
+            bits |= bits << num
+        
+        return (bits >> total//2) & 1 == 1
