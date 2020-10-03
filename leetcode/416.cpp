832dc4c1ac49b9b2652cf84e7e176bc612021a72
hwen <whman63@gmail.com>
2020-10-03 11:31:57 +0800

    bitset in cpp

---
+++ leetcode/416.cpp
@@ -0,0 +1,15 @@
+// 416. Partition Equal Subset Sum
+// https://leetcode.com/problems/partition-equal-subset-sum/
+
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        bitset<10001> bits(1);
+        int total = 0;
+        
+        for (auto n: nums)
+            bits |= bits << n, total += n;
+        
+        return !(total&1) && bits[total>>1];
+    }
+};
