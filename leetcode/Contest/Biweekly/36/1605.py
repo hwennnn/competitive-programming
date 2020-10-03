1f01ac668231d7eeb32ba4c8419f0173e09066d0
hwen <whman63@gmail.com>
2020-10-04 00:43:02 +0800

    biweekly 36 q1-q3

---
+++ leetcode/Contest/Biweekly/36/1605.py
@@ -0,0 +1,21 @@
+# 1605. Find Valid Matrix Given Row and Column Sums
+# https://leetcode.com/problems/find-valid-matrix-given-row-and-column-sums/
+
+class Solution:
+    def restoreMatrix(self, rs: List[int], cs: List[int]) -> List[List[int]]:
+        r = len(rs)
+        c = len(cs)
+        
+        lst = [[0]*c for _ in range(r)]
+        
+        for i in range(r):
+            for j in range(c):
+                
+                cur = min(rs[i], cs[j])
+                
+                lst[i][j] = cur
+                rs[i] -= cur
+                cs[j] -= cur
+
+        return lst
+                
