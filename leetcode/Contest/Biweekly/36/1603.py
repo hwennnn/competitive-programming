df3b7d4fed1ce22887d5a90274d43fa12106e625
hwen <whman63@gmail.com>
2020-10-04 00:47:52 +0800

    optimised version for biweekly 36 q1

--- leetcode/Contest/Biweekly/36/1603.py
+++ leetcode/Contest/Biweekly/36/1603.py
@@ -2,32 +2,15 @@
 # https://leetcode.com/problems/design-parking-system/
 
 class ParkingSystem:
-    lst = [0,0,0]
     def __init__(self, big: int, medium: int, small: int):
-        self.lst[0] = big
-        self.lst[1] = medium
-        self.lst[2] = small
+        self.lst = [big,medium,small]
 
     def addCar(self, carType: int) -> bool:
-        if carType == 1:
-            if self.lst[0] > 0:
-                self.lst[0] -= 1
-                return True
-            else:
-                return False
-        elif carType == 2:
-            if self.lst[1] > 0:
-                self.lst[1] -= 1
-                return True
-            else:
-                return False
-        elif carType == 3:
-            if self.lst[2] > 0:
-                self.lst[2] -= 1
-                return True
-            else:
-                return False
-
+        if self.lst[carType-1] > 0:
+            self.lst[carType-1] -= 1
+            return True
+        return False
+    
 # Your ParkingSystem object will be instantiated and called as such:
 # obj = ParkingSystem(big, medium, small)
 # param_1 = obj.addCar(carType)
