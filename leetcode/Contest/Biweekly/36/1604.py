522d34a2e28611ffc4e85c1c2ae2ccc001599af3
hwen <whman63@gmail.com>
2020-10-04 00:44:07 +0800

    comment

--- leetcode/Contest/Biweekly/36/1604.py
+++ leetcode/Contest/Biweekly/36/1604.py
@@ -1,5 +1,5 @@
-1604. Alert Using Same Key-Card Three or More Times in a One Hour Period
-https://leetcode.com/problems/alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/
+# 1604. Alert Using Same Key-Card Three or More Times in a One Hour Period
+# https://leetcode.com/problems/alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/
 
 class Solution:
     def alertNames(self, keyName: List[str], keyTime: List[str]) -> List[str]:
