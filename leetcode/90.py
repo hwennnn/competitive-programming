758ba533297ea78e56d63443da426b18d0c6a47a
hwen <whman63@gmail.com>
2020-10-03 17:18:08 +0800

    iterative sol for subsets II

---
+++ leetcode/90.py
@@ -0,0 +1,25 @@
+# 90. Subsets II
+# https://leetcode.com/problems/subsets-ii/
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        
+        n = len(nums)
+        nums.sort()
+        ans = [[]]
+        start = 1
+        
+        for i in range(n):
+            tmp = []
+            for j, num in enumerate(ans):
+                if i > 0 and nums[i] == nums[i-1] and j < start: continue
+                    
+                c = num + [nums[i]]
+                tmp.append(c)
+            
+            start = len(ans)
+            ans += tmp
+        
+        return ans
+                
+        
