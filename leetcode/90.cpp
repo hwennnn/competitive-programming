c810e0387fe4a1059fc779389a120f078dbea6a2
hwen <whman63@gmail.com>
2020-10-03 17:05:40 +0800

    90 lc

--- leetcode/90.cpp
+++ leetcode/90.cpp
@@ -1,6 +1,7 @@
 // 90. Subsets II
 // https://leetcode.com/problems/subsets-ii/
 
+// bitmasking
 class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
@@ -20,6 +21,36 @@ public:
                 res.push_back(c);
             }
                 
+        }
+        
+        return res;
+    }
+};
+
+// check and skip when repeated
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        vector<vector<int>> res;
+        
+        for (int i=0; i < 1<<n; ++i){
+            vector<int> c;
+            bool isRepeated = false;
+            for (int j = 0; j < n; ++j)
+                if (i >> j&1){
+                    if (j > 0 && nums[j]==nums[j-1] && (i>>(j-1)&1) == 0){
+                        isRepeated = true;
+                        break;
+                    }else{
+                        c.push_back(nums[j]);
+                    }
+                }
+                    
+            if (!isRepeated){
+                res.push_back(c);
+            }
         }
         
         return res;
