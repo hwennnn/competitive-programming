481dcf3358800e48855b35e1ddd71e96ce4cd0f8
hwen <whman63@gmail.com>
2020-10-04 20:20:36 +0800

    Sun  4 Oct 2020 20:20:36 +08

---
+++ leetcode/1288.py
@@ -0,0 +1,13 @@
+# 1288. Remove Covered Intervals
+# https://leetcode.com/problems/remove-covered-intervals/
+
+class Solution:
+    def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda a: (a[0], -a[1]))
+        res = right = 0
+        
+        for x,y in intervals:
+            res += y > right
+            right = max(right, y)
+
+        return res
