11e8e02cada9df6328d4550119f71fb108b8fcde
hwen <whman63@gmail.com>
2020-10-04 12:03:46 +0800

    weekly contest 209

---
+++ leetcode/Contest/Weekly/209/1609.py
@@ -0,0 +1,55 @@
+# 1609. Even Odd Tree
+# https://leetcode.com/problems/even-odd-tree/
+
+from collections import deque  
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def printLevelOrder(self, root): 
+
+        if root is None: 
+            return
+        
+        q = [] 
+        q.append(root) 
+        i = 0
+        while q: 
+
+            c = []
+            prev = None
+            count = len(q)
+
+            while count > 0: 
+                temp = q.pop(0) 
+                c.append(temp.val)
+                if temp.left: 
+                    q.append(temp.left) 
+                if temp.right: 
+                    q.append(temp.right) 
+
+                count -= 1
+                
+            for num in c:
+                if (i)%2 == 0:
+
+                    if num%2 == 0 or prev and num <= prev:
+                        return False
+
+                else:
+                    if num%2 or prev and num >= prev:
+                        return False
+
+                prev = num
+            i += 1
+            # print(c)
+        
+        return True
+    
+    def isEvenOddTree(self, root: TreeNode) -> bool:
+        return self.printLevelOrder(root)  
+        
