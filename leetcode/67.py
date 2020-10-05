37d965ee637b666e085f516c224df9c88d59fabe
hwen <whman63@gmail.com>
2020-10-05 20:25:27 +0800

    add binary

---
+++ leetcode/67.py
@@ -0,0 +1,20 @@
+# 67. Add Binary
+# https://leetcode.com/problems/add-binary/
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        i = len(a) - 1
+        j = len(b) - 1
+        carry = 0
+        res = []
+        
+        while (i >= 0 or j >= 0 or carry):
+            s = carry
+            s += int(a[i]) if i >= 0 else 0
+            s += int(b[j]) if j >= 0 else 0
+            res.insert(0, str(s%2))
+            carry = s//2
+            i -= 1
+            j -= 1
+            
+        return "".join(res)
