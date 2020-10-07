e86c980faadf04fc940e713698578f7de7a07da3
hwen <whman63@gmail.com>
2020-10-07 20:31:19 +0800

    128. Longest Consecutive Sequence Approach 2

--- leetcode/128.py
+++ leetcode/128.py
@@ -17,3 +17,25 @@ class Solution:
                 res = max(res, y - x)
         
         return res
+
+
+# Another approach: remove element once found
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        s = set(nums)
+        res = 0
+        
+        while s:
+            first = last = s.pop()
+            
+            while first - 1 in s:
+                first -= 1
+                s.remove(first)
+            
+            while last + 1 in s:
+                last += 1
+                s.remove(last)
+            
+            res = max(res, last - first + 1)
+        
+        return res
