f5a2c29f17c77acb07d67dc830e5a7aec4bc5a10
hwen <whman63@gmail.com>
2020-10-13 17:37:40 +0800

    sliding window with prefix and suffix sum for 1423. Maximum Points You Can Obtain from Cards

--- leetcode/1432.py
+++ leetcode/1432.py
@@ -21,3 +21,21 @@ class Solution:
                 res = min(res, c)
         
         return sum(arr) - res
+
+# Approach: sliding window with prefix and suffix sum
+    def maxScore(self, arr: List[int], k: int) -> int:
+            
+            prefix, suffix = [0], [0]
+            
+            for v in arr:
+                prefix.append(prefix[-1] + v)
+            
+            for v in arr[::-1]:
+                suffix.append(suffix[-1] + v)
+                
+            res = float("-inf")
+            
+            for i in range(k+1):
+                res = max(res, prefix[i] + suffix[k-i])
+            
+            return res
