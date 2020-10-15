0c51cf26a3766121ee8316802c240d3ecb409ab7
hwen <whman63@gmail.com>
2020-10-15 16:15:17 +0800

    1387. Sort Integers by The Power Value

---
+++ leetcode/1387.cpp
@@ -0,0 +1,26 @@
+// 1387. Sort Integers by The Power Value
+// https://leetcode.com/problems/sort-integers-by-the-power-value/
+
+class Solution {
+    #define ar array
+public:
+    int getKth(int lo, int hi, int k) {
+        vector<ar<int,2>> mp;
+        
+        for (int i = lo; i <= hi; i++){
+            mp.push_back({0,i});
+            int j = i;
+            while (j > 1){
+                if (j&1)
+                    j = 3 * j + 1;
+                else
+                    j = j / 2;
+                ++mp.back()[0];
+            }
+        }
+        
+        sort(mp.begin(), mp.end());
+        
+        return mp[k-1][1];
+    }
+};
