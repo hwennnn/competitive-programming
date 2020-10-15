333980c529c9a38a2d1b2e164ba5c1135049b04b
hwen <whman63@gmail.com>
2020-10-16 00:53:38 +0800

    1376. Time Needed to Inform All Employees

---
+++ leetcode/1376.py
@@ -0,0 +1,15 @@
+# 1376. Time Needed to Inform All Employees
+# https://leetcode.com/problems/time-needed-to-inform-all-employees/
+
+class Solution:
+    def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
+        children = [[] for _ in range(n)]
+        
+        for i, m in enumerate(manager):
+            if m >= 0:
+                children[m].append(i)
+        
+        def dfs(i):
+            return max([dfs(j) for j in children[i]] or [0]) + informTime[i]
+        
+        return dfs(headID)
