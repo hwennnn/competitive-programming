f1668070679743268526dc30a566bf49fda35a10
hwen <whman63@gmail.com>
2020-10-15 16:13:05 +0800

    189. Rotate Array

---
+++ leetcode/189.cpp
@@ -0,0 +1,15 @@
+// 189. Rotate Array
+// https://leetcode.com/problems/rotate-array/
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k %= n;
+        
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());
+        
+    }
+};
