c9a1d174088e2b6ad64534562c53b33b2ee584e6
hwen <whman63@gmail.com>
2020-10-16 13:53:52 +0800

    1366. Rank Teams by Votes

---
+++ leetcode/1366.py
@@ -0,0 +1,12 @@
+# 1366. Rank Teams by Votes
+# https://leetcode.com/problems/rank-teams-by-votes/
+
+class Solution:
+    def rankTeams(self, votes: List[str]) -> str:
+        mp = {v: [0] * len(votes[0]) + [v] for v in votes[0]}
+        
+        for v in votes:
+            for i,t in enumerate(v):
+                mp[t][i] -= 1
+        
+        return "".join(sorted(votes[0], key = mp.get))
