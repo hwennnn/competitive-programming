04bf28f7ef4450ed34144987ba2fae23d1c2348f
hwen <whman63@gmail.com>
2020-10-16 21:06:59 +0800

    347. Top K Frequent Elements

---
+++ leetcode/347.cpp
@@ -0,0 +1,30 @@
+// 347. Top K Frequent Elements
+// https://leetcode.com/problems/top-k-frequent-elements/
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        
+        for (int num: nums)
+            ++mp[num];
+        
+        
+        vector<vector<int>> buckets(nums.size() + 1); 
+        for (auto v: mp)
+            buckets[v.second].push_back(v.first);
+        
+        
+        vector<int> res;
+        for (int i = buckets.size() - 1; i >= 0 && res.size() < k; --i){
+            for (int num : buckets[i]){
+                res.push_back(num);
+                
+                if (res.size() == k)
+                    break;
+            }
+        }
+        
+        return res;
+    }
+};
---
+++ leetcode/347.py
@@ -0,0 +1,15 @@
+# 347. Top K Frequent Elements
+# https://leetcode.com/problems/top-k-frequent-elements/
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        buckets = [[] for _ in range(len(nums)+1)]
+        
+        c = collections.Counter(nums).items()
+
+        for num,freq in c:
+            buckets[freq].append(num)
+
+        arr = list(chain(*buckets))
+
+        return arr[::-1][:k]
