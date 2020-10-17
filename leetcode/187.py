0244c8203ba613ae33fcf0d7a96e195e7eea414e
hwen <whman63@gmail.com>
2020-10-17 15:22:11 +0800

    187. Repeated DNA Sequences

---
+++ leetcode/187.py
@@ -0,0 +1,18 @@
+# 187. Repeated DNA Sequences
+# https://leetcode.com/problems/repeated-dna-sequences/
+
+class Solution:
+    def findRepeatedDnaSequences(self, S: str) -> List[str]:
+        n = len(S)
+        if n <= 10: return []
+        
+        mp = collections.defaultdict(int)
+        
+        res = []
+        for i in range(n-9):
+            key = S[i:i+10]
+            mp[key] += 1
+            if mp[key] == 2:
+                res.append(key)
+        
+        return res
