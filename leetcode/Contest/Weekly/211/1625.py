fa54647e968e9bd2bd0e101e9423821a4252985b
hwen <whman63@gmail.com>
2020-10-18 15:05:16 +0800

    1625. Lexicographically Smallest String After Applying Operations

---
+++ leetcode/Contest/Weekly/211/1625.py
@@ -0,0 +1,29 @@
+# 1625. Lexicographically Smallest String After Applying Operations
+# https://leetcode.com/problems/lexicographically-smallest-string-after-applying-operations/
+
+# bfs
+class Solution:
+    def findLexSmallestString(self, s: str, a: int, b: int) -> str:
+        n = len(s)
+        deq, seen, res = deque([s]), {s}, s
+        
+        while deq:
+            c = deq.popleft()
+            res = min(res, c)
+            A = list(c)
+            
+            for i in range(1,n,2):
+                A[i] = str(((int(A[i])+a)%10))
+            
+            A = "".join(A)
+            if A not in seen:
+                seen.add(A)
+                deq.append(A)
+            
+            rotate = c[b:] + c[:b]
+            if rotate not in seen:
+                seen.add(rotate)
+                deq.append(rotate)
+        
+        return res
+                
