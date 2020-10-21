603d6948a56331f966cb353b54194cb7b02cee95
hwen <whman63@gmail.com>
2020-10-21 20:15:16 +0800

    735. Asteroid Collision

---
+++ leetcode/735.py
@@ -0,0 +1,25 @@
+# 735. Asteroid Collision
+# https://leetcode.com/problems/asteroid-collision/
+
+class Solution:
+    def asteroidCollision(self, A: List[int]) -> List[int]:
+        stack = []
+        
+        for a in A:
+            
+            while stack and stack[-1] > 0 and a < 0:
+                if stack[-1] == -a:
+                    stack.pop()
+                    break
+                    
+                elif stack[-1] > -a:
+                    break
+                
+                elif stack[-1] < -a:
+                    stack.pop()
+            
+            else:
+                stack.append(a)
+        
+        return stack
+                
