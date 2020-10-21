cf3aca505783766b6aafc8b3bfe42d2b705b07c6
hwen <whman63@gmail.com>
2020-10-21 20:14:39 +0800

    5. Longest Palindromic Substring

---
+++ leetcode/5.py
@@ -0,0 +1,17 @@
+# 5. Longest Palindromic Substring
+# https://leetcode.com/problems/longest-palindromic-substring/
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        def helper(i,j):
+            while i >= 0 and j < len(s) and s[i] == s[j]:
+                i -= 1
+                j += 1
+            
+            return s[i+1:j]
+        
+        res = ""
+        for i in range(len(s)):
+            res = max(res,helper(i,i), helper(i,i+1), key = len)
+        
+        return res
