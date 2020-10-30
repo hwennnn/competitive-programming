e628f1ed6d62ffbd306e352c0e89c0ab23c2ab23
hwen <whman63@gmail.com>
2020-10-30 20:45:32 +0800

    300. Longest Increasing Subsequence

---
+++ leetcode/300.py
@@ -0,0 +1,18 @@
+# 300. Longest Increasing Subsequence
+# https://leetcode.com/problems/longest-increasing-subsequence/
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        res = 1
+        n = len(nums)
+        if n == 0: return 0
+        dp = [1] * n
+        
+        for i in range(1, n):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j]+1)
+            
+            res = max(res, dp[i])
+        
+        return res
