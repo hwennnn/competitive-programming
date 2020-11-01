d9e077d26883d17bc2532399dac3e95cb401fe6d
hwen <whman63@gmail.com>
2020-11-01 12:12:35 +0800

    1640. Check Array Formation Through Concatenation

---
+++ leetcode/Contest/Weekly/213/1640.py
@@ -0,0 +1,16 @@
+# 1640. Check Array Formation Through Concatenation
+# https://leetcode.com/problems/check-array-formation-through-concatenation/
+
+class Solution:
+    def canFormArray(self, arr: List[int], pieces: List[List[int]]) -> bool:
+        n1 = len(arr)
+        n2 = len(pieces)
+        
+        tmp = []
+        for i in range(n1):
+            tmp.append(arr[i])
+
+            if tmp in pieces:
+                tmp = []
+        
+        return len(tmp) == 0
