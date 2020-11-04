06af6f7f8babf4a403515bc415629434d639dd04
hwen <whman63@gmail.com>
2020-11-04 18:25:24 +0800

    1642. Furthest Building You Can Reach

--- leetcode/Contest/Weekly/213/1642.py
+++ leetcode/Contest/Weekly/213/1642.py
@@ -4,31 +4,22 @@
 class Solution:
     def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
         
-        n = len(heights)    
-        m = float("inf")
-        res = 0
+        n = len(heights)
+        heap = []
         
         for i in range(n-1):
-            if heights[i] >= heights[i+1]:
-                res += 1
-            else:
-                d = heights[i+1] - heights[i]
-                optimald = d
-                if d > m:
-                    optimald = m
-                    m = d
-
-                if bricks >= optimald:
-                    res += 1
-                    bricks -= optimald
-                else:
-                    if ladders > 0:
-                        ladders -= 1
-                        res += 1
-                        m = d
-                    else:
-                        break
+            d = heights[i+1] - heights[i]
+            
+            if d > 0:
+                heapq.heappush(heap,d)
+            
+            if len(heap) > ladders:
+                bricks -= heapq.heappop(heap)
+            
+            if bricks < 0:
+                return i
         
-        return res
+        return n - 1
+            
     
             
