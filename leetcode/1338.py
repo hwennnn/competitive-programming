c99459d6d119d02b7e4e4bbbc4c6a5375bb4e41d
hwen <whman63@gmail.com>
2020-11-10 23:11:08 +0800

    1338. Reduce Array Size to The Half

---
+++ leetcode/1338.py
@@ -0,0 +1,22 @@
+# 1338. Reduce Array Size to The Half
+# https://leetcode.com/problems/reduce-array-size-to-the-half/
+
+class Solution:
+    def minSetSize(self, arr: List[int]) -> int:
+        c = Counter(arr)
+        v = sorted(c.values(), reverse = True)
+        
+        n = len(arr)
+        half = n // 2
+        res = 0
+        
+        for x in v:
+            n -= x
+            res += 1
+            
+            if n <= half:
+                return res
+        
+        return res
+        
+        
