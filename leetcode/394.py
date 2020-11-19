4da959b70db0febda3a13f4ee20f95d153eff32e
hwen <whman63@gmail.com>
2020-11-19 20:29:53 +0800

    394. Decode String

---
+++ leetcode/394.py
@@ -0,0 +1,26 @@
+# 394. Decode String
+# https://leetcode.com/problems/decode-string/
+
+class Solution:
+    def decodeString(self, s: str) -> str:
+        n = len(s)
+        stack = []
+        
+        for c in s:
+            if c == "]":
+                t = ""
+                while stack[-1] != "[":
+                    t = stack.pop() + t
+                
+                stack.pop()
+                
+                times = ""
+                while stack and stack[-1].isnumeric():
+                    times = stack.pop() + times
+                
+                stack.append(int(times)*t)
+            
+            else:
+                stack.append(c)
+        
+        return "".join(stack)
