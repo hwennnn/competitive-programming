06a9c61875dc3b46afb2e2e3f54bae072d04afef
hwen <whman63@gmail.com>
2020-11-21 15:49:43 +0800

    1654. Minimum Jumps to Reach Home

--- leetcode/Contest/Biweekly/39/1654.py
+++ leetcode/Contest/Biweekly/39/1654.py
@@ -4,25 +4,23 @@
 class Solution:
     def minimumJumps(self, forbidden: List[int], a: int, b: int, x: int) -> int:
         
-        max_val=max([x]+forbidden) + a+b
+        seen = set()
+        f = set(forbidden)
+        max_val = max([x]+forbidden) + a + b
+        deq = collections.deque([(0,0,0)]) # now, count, isBackward
         
-        jumps=[0] + [math.inf] * (max_val)
-        for pos in forbidden: jumps[pos] = -1
+        while deq:
+            now, count, isBackward = deq.popleft()
+            
+            if now == x:
+                return count
+            
+            if now + a not in f and now + a <= max_val and (now+a, False) not in seen:
+                deq.append((now+a, count+1, False))
+                seen.add((now+a, False))
+            
+            if now - b not in f and now - b >= 0 and not isBackward and (now-b, isBackward) not in seen:
+                deq.append((now-b, count+1, True))
+                seen.add((now-b, True))
         
-        stack=deque([0])
-        
-        while stack:
-            pos=stack.popleft()
-
-            if pos+a<= max_val and jumps[pos+a]>jumps[pos]+1:
-                stack.append(pos+a)
-                jumps[pos+a]=jumps[pos]+1
-                
-            if pos-b>0 and  jumps[pos-b]>jumps[pos]+1:
-                jumps[pos-b]=jumps[pos]+1
-                
-                if pos-b+a<= max_val and jumps[pos-b+a]>jumps[pos]+2:
-                    stack.append(pos-b+a)
-                    jumps[pos-b+a]=jumps[pos]+2
-                    
-        return jumps[x] if jumps[x]<math.inf else -1
+        return -1
