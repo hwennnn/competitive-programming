45c8bfada4fc94c1cbac61a259e6c385f1e808d1
hwen <whman63@gmail.com>
2020-12-05 16:17:33 +0800

    605. Can Place Flowers

---
+++ leetcode/605.py
@@ -0,0 +1,18 @@
+# 605. Can Place Flowers
+# https://leetcode.com/problems/can-place-flowers/
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        
+        plots = [0] + flowerbed + [0]
+        
+        i = 1
+        
+        while i<= len(flowerbed) and n>0:
+            if plots[i] == 0 and plots[i-1] == 0 and plots[i+1] == 0:
+                plots[i] = 1
+                n-=1
+            
+            i += 1
+            
+        return n == 0
