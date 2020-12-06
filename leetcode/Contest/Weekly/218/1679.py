7adbb4b8144130dd95340fe43a2d4a01089438b9
hwen <whman63@gmail.com>
2020-12-06 22:17:12 +0800

    1679. Max Number of K-Sum Pairs

--- leetcode/Contest/Weekly/218/1679.py
+++ leetcode/Contest/Weekly/218/1679.py
@@ -19,3 +19,20 @@ class Solution:
                 mp[x] += 1
         
         return res
+    
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        
+        c = collections.Counter(nums)
+        res = 0
+        
+        for x in nums:
+            if c[x] > 0 and c[k-x] > 0:
+                c[x] -= 1
+                c[k-x] -= 1
+                res += 1
+                
+                if c[x] < 0:
+                    res -= 1
+        
+        return res
+        
