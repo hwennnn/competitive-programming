822ca99bf9747a6f57c9dc2e8ae510914d707a78
hwen <whman63@gmail.com>
2020-12-22 22:07:28 +0800

    910. Smallest Range II

---
+++ leetcode/910.py
@@ -0,0 +1,16 @@
+# 910. Smallest Range II
+# https://leetcode.com/problems/smallest-range-ii/
+
+class Solution:
+    def smallestRangeII(self, A: List[int], K: int) -> int:
+        A.sort()
+        
+        res = A[-1] - A[0]
+        
+        for i in range(len(A) - 1):
+            maxi = max(A[i] + K, A[-1] - K)
+            mini = min(A[0] + K, A[i+1] - K)
+            
+            res = min(res, maxi - mini)
+        
+        return res
