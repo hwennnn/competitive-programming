889ba01830279f09f4826cc3e13cc6da23225803
hwen <whman63@gmail.com>
2020-12-27 22:53:23 +0800

    1302. Deepest Leaves Sum

---
+++ leetcode/Contest/Biweekly/16/1302.py
@@ -0,0 +1,28 @@
+# 1302. Deepest Leaves Sum
+# https://leetcode.com/problems/deepest-leaves-sum/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deepestLeavesSum(self, root: TreeNode) -> int:
+        deq = collections.deque([root])
+        res = 0
+        
+        while deq:
+            l = len(deq)
+            res = 0
+            
+            while l:
+                node = deq.popleft()
+                res += node.val
+                
+                for n in (node.left, node.right):
+                    if n: deq.append(n)
+                l -= 1
+            
+
+        return res
