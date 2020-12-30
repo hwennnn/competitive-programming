e8bd8b3b47081936c86918e1d8c755dfdbb0f6f5
hwen <whman63@gmail.com>
2020-12-30 22:50:18 +0800

    1011. Capacity To Ship Packages Within D Days

---
+++ leetcode/1011.py
@@ -0,0 +1,32 @@
+# 1011. Capacity To Ship Packages Within D Days
+# https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/
+
+class Solution:
+    def shipWithinDays(self, weights: List[int], D: int) -> int:
+        
+        def good(x):
+            total = 0
+            day = 1
+            
+            for w in weights:
+                total += w
+                if total > x:
+                    total = w
+                    day += 1
+                
+                if day > D:
+                    return False
+        
+            return True
+    
+        left, right = max(weights), sum(weights)
+        
+        while left < right:
+            mid = (left+right) // 2
+            
+            if good(mid):
+                right = mid
+            else:
+                left = mid + 1
+        
+        return left
