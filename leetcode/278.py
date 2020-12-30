2723eadc92ba612a55596c868db216caf7312297
hwen <whman63@gmail.com>
2020-12-30 22:09:56 +0800

    278. First Bad Version

---
+++ leetcode/278.py
@@ -0,0 +1,21 @@
+# 278. First Bad Version
+# https://leetcode.com/problems/first-bad-version/
+
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return an integer
+# def isBadVersion(version):
+
+class Solution:
+    def firstBadVersion(self, n):
+        left, right = 1, n
+        
+        while left < right:
+            mid = (left+right)//2
+            
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+        
+        return left
