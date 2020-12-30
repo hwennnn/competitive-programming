9b6afcf28c00f225a64d7ed94d721be52c40cf91
hwen <whman63@gmail.com>
2020-12-30 22:29:03 +0800

    69. Sqrt(x)

---
+++ leetcode/69.py
@@ -0,0 +1,20 @@
+# 69. Sqrt(x)
+# https://leetcode.com/problems/sqrtx/
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x <= 1: return x
+        
+        def good(v):
+            return v*v <= x
+        
+        left, right = 0, x
+        
+        while left < right:
+            mid = left + (right - left) // 2
+            if good(mid):
+                left = mid + 1
+            else:
+                right = mid
+        
+        return left - 1
