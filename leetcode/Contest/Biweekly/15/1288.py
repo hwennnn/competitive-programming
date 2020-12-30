c5db1dd13affa1e59935f37069cee757149b01f7
hwen <whman63@gmail.com>
2020-12-30 21:20:31 +0800

    1288. Remove Covered Intervals

--- leetcode/Contest/Biweekly/15/1288.py
+++ leetcode/Contest/Biweekly/15/1288.py
@@ -21,3 +21,14 @@ class Solution:
             if check: res += 1
         
         return res
+
+    def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
+        res = right = 0
+        
+        intervals.sort(key = lambda x:(x[0],-x[1]))
+        
+        for start,end in intervals:
+            res += end > right
+            right = max(right, end)
+        
+        return res
