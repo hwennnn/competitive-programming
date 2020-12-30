1d1aa5dcbffb92d63fce8f8419ee6ccc0f4f8a93
hwen <whman63@gmail.com>
2020-12-30 23:13:19 +0800

    875. Koko Eating Bananas

---
+++ leetcode/875.py
@@ -0,0 +1,25 @@
+# 875. Koko Eating Bananas
+# https://leetcode.com/problems/koko-eating-bananas/
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], H: int) -> int:
+        def good(x):
+            hours = 0
+            for p in piles:
+                r = ceil(p/x)
+                hours += r
+                if hours > H: return False
+            
+            return True
+        
+        left, right = 1, max(piles)
+        
+        while left < right:
+            mid = (left+right) // 2
+            
+            if good(mid):
+                right = mid
+            else:
+                left = mid + 1
+        
+        return left
