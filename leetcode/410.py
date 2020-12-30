b2f03b097b6c9774a14fb59aa6b35832d303fa2d
hwen <whman63@gmail.com>
2020-12-30 22:55:07 +0800

    410. Split Array Largest Sum

---
+++ leetcode/410.py
@@ -0,0 +1,31 @@
+# 410. Split Array Largest Sum
+# https://leetcode.com/problems/split-array-largest-sum/
+
+class Solution:
+    def splitArray(self, nums: List[int], m: int) -> int:
+        
+        def good(x):
+            total = 0
+            a = 1
+            
+            for num in nums:
+                total += num
+                if total > x:
+                    total = num
+                    a += 1
+                
+                if a > m: return False
+            
+            return True
+        
+        left, right = max(nums), 10**8
+        
+        while left < right:
+            mid = (left+right) // 2
+            
+            if good(mid):
+                right = mid
+            else:
+                left = mid + 1
+        
+        return left
