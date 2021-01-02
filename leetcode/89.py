301ae4c1f4f61f96fb245c40296287f1494db21b
hwen <whman63@gmail.com>
2021-01-02 21:38:34 +0800

    89. Gray Code

--- leetcode/89.py
+++ leetcode/89.py
@@ -2,5 +2,14 @@
 # https://leetcode.com/problems/gray-code/
 
 class Solution:
-    def grayCode(self, n: int) -> List[int]:
+    def grayCode(self, n: int):
         return [i ^ i >> 1 for i in range(1 << n)]
+
+    def grayCode(self, n: int):
+        res = [0]
+        
+        for i in range(n):
+            for j in range(len(res)-1,-1,-1):
+                res.append(res[j] | 1 << i)
+        
+        return res
