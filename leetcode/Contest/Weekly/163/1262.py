113dd00641a2b8ee5254d0eeab6ff1f9b5eadf40
hwen <whman63@gmail.com>
2021-01-02 13:21:31 +0800

    1262. Greatest Sum Divisible by Three

---
+++ leetcode/Contest/Weekly/163/1262.py
@@ -0,0 +1,16 @@
+# 1262. Greatest Sum Divisible by Three
+# https://leetcode.com/problems/greatest-sum-divisible-by-three/
+
+class Solution:
+    def maxSumDivThree(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [0, 0, 0]
+        
+        for num in nums:
+            
+            for i in dp[:]:
+                dp[(i+num)%3] = max(dp[(i+num)%3], num + i)
+            
+        
+        return dp[0]
+    
