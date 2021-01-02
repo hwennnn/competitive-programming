082650f7e38d7f7a91587dcf76bcc69b17ad2434
hwen <whman63@gmail.com>
2021-01-02 16:59:10 +0800

    1248. Count Number of Nice Subarrays

--- leetcode/Contest/Weekly/161/1248.py
+++ leetcode/Contest/Weekly/161/1248.py
@@ -2,7 +2,24 @@
 # https://leetcode.com/problems/count-number-of-nice-subarrays/
 
 class Solution:
-    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+    def numberOfSubarrays(self, nums: List[int], k: int):
+        res = i = count = 0
+        
+        for j,x in enumerate(nums):
+            if x & 1:
+                k -= 1
+                count = 0
+            
+            while k == 0:
+                k += nums[i] & 1
+                i += 1
+                count += 1
+            
+            res += count
+
+        return res
+    
+    def numberOfSubarrays(self, nums: List[int], k: int):
         n = len(nums)
         prefix = [0]
         start = float('inf')
