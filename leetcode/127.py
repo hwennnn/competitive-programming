2643e42f137a9fa2f4ed59d623d36a15907f5cec
hwen <whman63@gmail.com>
2021-01-09 16:56:45 +0800

    127. Word Ladder

--- leetcode/127.py
+++ leetcode/127.py
@@ -38,6 +38,25 @@ class Solution:
 
     def ladderLength(self, begin: str, end: str, wordList: List[str]):
         s = set(wordList)
+        deq = collections.deque([(begin, 1)])
+        visited = set()
+        
+        while deq:
+            word, count = deq.popleft()
+            if word == end: return count
+            
+            for i in range(len(word)):
+                for x in string.ascii_lowercase:
+                    w = word[:i] + x + word[i+1:] 
+
+                    if w in s and w not in visited:
+                        visited.add(w)
+                        deq.append((w, count+1))
+        
+        return 0
+    
+    def ladderLength(self, begin: str, end: str, wordList: List[str]):
+        s = set(wordList)
         if end not in s: return 0
         
         def diff(w, word):
