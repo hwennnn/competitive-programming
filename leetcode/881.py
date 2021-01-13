123261111eed3611bba9b5f2c6a5942faf6e5e95
hwen <whman63@gmail.com>
2021-01-13 16:08:36 +0800

    881. Boats to Save People

---
+++ leetcode/881.py
@@ -0,0 +1,23 @@
+# 881. Boats to Save People
+# https://leetcode.com/problems/boats-to-save-people/
+
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int):
+        n = len(people)
+        if n == 1: return 1
+        
+        people.sort(reverse = 1)
+        i, j = 0, n-1
+        boat = 0
+        
+        while i <= j:
+            if i != j and people[i] + people[j] <= limit:
+                i += 1
+                j -= 1
+                boat += 1
+            else:
+                i += 1
+                boat += 1
+
+        return boat
+        
