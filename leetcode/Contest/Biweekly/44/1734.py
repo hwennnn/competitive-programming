dfb09304127045292cb8b98e5180712f36891a89
hwen <whman63@gmail.com>
2021-01-24 00:58:45 +0800

    1734. Decode XORed Permutation

---
+++ leetcode/Contest/Biweekly/44/1734.py
@@ -0,0 +1,19 @@
+# 1734. Decode XORed Permutation
+# https://leetcode.com/problems/decode-xored-permutation/
+
+class Solution:
+    def decode(self, encoded: List[int]):
+        curr = start = 0
+        
+        for i in range(1, len(encoded)+2):
+            start ^= i
+        
+        for x in encoded:
+            curr ^= x
+            start ^= curr
+        
+        res = [start]
+        for x in encoded:
+            res.append(res[-1] ^ x)
+        
+        return res
