9d742cf4fb49f18115dc2ba81bfb95288f240aa8
hwen <whman63@gmail.com>
2021-02-13 23:38:39 +0800

    594. Longest Harmonious Subsequence

---
+++ leetcode/594.py
@@ -0,0 +1,13 @@
+# 594. Longest Harmonious Subsequence
+# https://leetcode.com/problems/longest-harmonious-subsequence
+
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        cnt = collections.Counter(nums)
+        res = 0
+        
+        for num in cnt:
+            if num + 1 in cnt:
+                res = max(res, cnt[num] + cnt[num + 1])
+        
+        return res
