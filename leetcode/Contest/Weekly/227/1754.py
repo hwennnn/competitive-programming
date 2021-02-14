c9c83cf9d0950ae1073076a7ee9e739f0b4500a3
hwen <whman63@gmail.com>
2021-02-14 21:48:14 +0800

    1754. Largest Merge Of Two Strings

--- leetcode/Contest/Weekly/227/1754.py
+++ leetcode/Contest/Weekly/227/1754.py
@@ -1,20 +1,18 @@
 # 1754. Largest Merge Of Two Strings
-# https://leetcode.com/problems/largest-merge-of-two-strings/
+# https://leetcode.com/problems/largest-merge-of-two-strings
 
 class Solution:
     def largestMerge(self, word1: str, word2: str) -> str:
-        deq1 = collections.deque(list(word1))
-        deq2 = collections.deque(list(word2))
+        res = ""
         
-        res = []
-        
-        while deq1 and deq2:
-            if "".join(list(deq1)) >= "".join(list(deq2)):
-                res.append(deq1.popleft())
+        while word1 and word2:
+            if word1 >= word2:
+                res += word1[0]
+                word1 = word1[1:]
             else:
-                res.append(deq2.popleft())
+                res += word2[0]
+                word2 = word2[1:]
         
-        leftover = list(deq1 or deq2)
-        res += leftover
+        res += word1 or word2
         
-        return "".join(res)
+        return res
