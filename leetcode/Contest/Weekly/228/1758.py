01ade271e34e7dc7b3361ce9caed05e6619de26d
hwen <whman63@gmail.com>
2021-02-14 22:08:27 +0800

    1758. Minimum Changes To Make Alternating Binary String

---
+++ leetcode/Contest/Weekly/228/1758.py
@@ -0,0 +1,19 @@
+# 1758. Minimum Changes To Make Alternating Binary String
+# https://leetcode.com/problems/minimum-changes-to-make-alternating-binary-string
+
+class Solution:
+    def minOperations(self, s: str) -> int:
+        
+        n = len(s)
+        
+        first, second = ["1"], ["0"]
+        
+        for _ in range(n-1):
+            first.append(second[-1])
+            second.append(first[-2])
+        
+        def diff(word):
+            return sum(a != b for a,b in zip(word, s))
+        
+        return min(diff(first), diff(second))
+            
