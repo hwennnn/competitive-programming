b123db6e349fcbd8bda346ab939fb432f3beeb54
hwen <whman63@gmail.com>
2021-02-14 11:14:37 +0800

    1. Two Sum

---
+++ leetcode/1.py
@@ -0,0 +1,12 @@
+# 1. Two Sum
+# https://leetcode.com/problems/two-sum
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mp = collections.defaultdict(int)
+        
+        for i,x in enumerate(nums):
+            if target - x in mp:
+                return [mp[target-x], i]
+            
+            mp[x] = i
