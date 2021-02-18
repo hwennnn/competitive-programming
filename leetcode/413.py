8d175aa4fa0fde4a3e108440fd01aaa6474b362d
hwen <whman63@gmail.com>
2021-02-18 16:17:03 +0800

    413. Arithmetic Slices

---
+++ leetcode/413.py
@@ -0,0 +1,16 @@
+# 413. Arithmetic Slices
+# https://leetcode.com/problems/arithmetic-slices
+
+class Solution:
+    def numberOfArithmeticSlices(self, A: List[int]):
+        n = len(A)
+        curr = res = 0
+        
+        for i in range(2, n):
+            if A[i] - A[i-1] == A[i-1] - A[i-2]:
+                curr += 1
+                res += curr
+            else:
+                curr = 0
+        
+        return res
