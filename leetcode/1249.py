e8fa5bb78fce3d613f70731a7139f5544ff8ea60
hwen <whman63@gmail.com>
2021-02-19 16:26:18 +0800

    1249. Minimum Remove to Make Valid Parentheses

---
+++ leetcode/1249.py
@@ -0,0 +1,47 @@
+# 1249. Minimum Remove to Make Valid Parentheses
+# https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
+
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        res = ""
+        stack = []
+        mp = {}
+        
+        for i,c in enumerate(s):
+            if c == "(" or c == ")":
+                if c == ")" and stack and stack[-1][1] == "(":
+                    x, p = stack.pop()
+                    mp[x] = p
+                    mp[i] = c
+                else:
+                    stack.append((i,c))
+        
+        
+        for i,c in enumerate(s):
+            if c == "(" or c == ")":
+                if i in mp:
+                    res += mp[i]
+            else:
+                res += c
+        
+        return res
+    
+    def minRemoveToMakeValid(self, s: str) -> str:
+        stack = []
+        s = list(s)
+​
+        for i,c in enumerate(s):
+            if c == ")":
+                if stack: 
+                    stack.pop()
+                else:
+                    s[i] = ""
+​
+            elif c == "(":
+                stack.append(i)
+​
+​
+        while stack:
+            s[stack.pop()] = ""
+​
+        return "".join(s)
