5cb9d7ac3bf2db936e8e94db557d6062c797f2ec
hwen <whman63@gmail.com>
2021-02-21 16:15:08 +0800

    991. Broken Calculator

---
+++ leetcode/991.py
@@ -0,0 +1,12 @@
+# 991. Broken Calculator
+# https://leetcode.com/problems/broken-calculator
+
+class Solution:
+    def brokenCalc(self, X: int, Y: int):
+        res = 0
+        
+        while Y > X:
+            Y = Y // 2 if Y & 1 == 0 else Y + 1
+            res += 1
+                
+        return res + X - Y
