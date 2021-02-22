a023810c3ffaf19a107f9c8444185d49785442d1
hwen <whman63@gmail.com>
2021-02-22 16:30:36 +0800

    524. Longest Word in Dictionary through Deleting

---
+++ leetcode/524.py
@@ -0,0 +1,17 @@
+# 524. Longest Word in Dictionary through Deleting
+# https://leetcode.com/problems/longest-word-in-dictionary-through-deleting/
+
+class Solution:
+    def findLongestWord(self, s: str, d: List[str]) -> str:
+        d.sort(key = lambda x: (-len(x), x))
+        
+        for word in d:
+            i = 0
+            
+            for c in s:
+                if i < len(word) and c == word[i]:
+                    i += 1
+                
+            if i == len(word): return word
+        
+        return ""
