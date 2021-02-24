255ab25ead076151305adb28eeda3ff50e801b02
hwen <whman63@gmail.com>
2021-02-24 16:46:17 +0800

    856. Score of Parentheses

---
+++ leetcode/856.py
@@ -0,0 +1,16 @@
+# 856. Score of Parentheses
+# https://leetcode.com/problems/score-of-parentheses
+
+class Solution:
+    def scoreOfParentheses(self, S: str) -> int:
+        stack = []
+        curr = 0
+        
+        for c in S:
+            if c == "(":
+                stack.append(curr)
+                curr = 0
+            else:
+                curr = stack.pop() + max(curr*2, 1)
+        
+        return curr
