3797a86f8229d336b3d515e290963c7c17ca37ab
hwen <whman63@gmail.com>
2021-02-28 16:17:54 +0800

    895. Maximum Frequency Stack

---
+++ leetcode/895.py
@@ -0,0 +1,29 @@
+# 895. Maximum Frequency Stack
+# https://leetcode.com/problems/maximum-frequency-stack/
+
+class FreqStack:
+​
+    def __init__(self):
+        self.freq = collections.Counter()
+        self.m = collections.defaultdict(list)
+        self.maxf = 0
+​
+    def push(self, x: int) -> None:
+        freq, m = self.freq, self.m
+        freq[x] += 1
+        self.maxf = max(self.maxf, freq[x])
+        self.m[freq[x]].append(x)
+        
+​
+    def pop(self) -> int:
+        freq, m, maxf = self.freq, self.m, self.maxf
+        x = m[maxf].pop()
+        if not m[maxf]: self.maxf = maxf - 1
+        freq[x] -= 1
+        return x
+​
+​
+# Your FreqStack object will be instantiated and called as such:
+# obj = FreqStack()
+# obj.push(x)
+# param_2 = obj.pop()
