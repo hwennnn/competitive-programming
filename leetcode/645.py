b6b541cecf51f53ca3e084351c816d798f3b48c8
hwen <whman63@gmail.com>
2021-03-02 19:17:34 +0800

    645. Set Mismatch

---
+++ leetcode/645.py
@@ -0,0 +1,16 @@
+# 645. Set Mismatch
+# https://leetcode.com/problems/set-mismatch/
+
+class Solution:
+    def findErrorNums(self, nums: List[int]):
+        n = len(nums)
+        mp = collections.Counter(nums)
+        
+        twice = zero = None
+        
+        for i in range(1,n+1):
+            if mp[i] == 0: zero = i
+            elif mp[i] == 2: twice = i
+        
+        return [twice,zero]
+        
