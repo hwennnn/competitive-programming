a2ef826a6c6a7ee2afb32cb3a341e229bd6c3ea8
hwen <whman63@gmail.com>
2021-03-06 22:58:41 +0800

    820. Short Encoding of Words

---
+++ leetcode/820.py
@@ -0,0 +1,12 @@
+# 820. Short Encoding of Words
+# https://leetcode.com/problems/short-encoding-of-words/
+
+class Solution:
+    def minimumLengthEncoding(self, words: List[str]) -> int:
+        s = set(words)
+        
+        for w in words:
+            for i in range(1, len(w)):
+                s.discard(w[i:])
+        
+        return sum(len(w) + 1 for w in s)
