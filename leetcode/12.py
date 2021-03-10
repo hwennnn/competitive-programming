5406932f10a48e91fe01c5ddbd9f59de0906f0f9
hwen <whman63@gmail.com>
2021-03-10 17:38:00 +0800

    12. Integer to Roman

---
+++ leetcode/12.py
@@ -0,0 +1,14 @@
+# 12. Integer to Roman
+# https://leetcode.com/problems/integer-to-roman
+
+class Solution:
+    def intToRoman(self, num):
+        mp = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+        nums = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        result = ""
+        
+        for letter, n in zip(mp, nums):
+            result += letter * (num // n)
+            num %= n
+            
+        return result
