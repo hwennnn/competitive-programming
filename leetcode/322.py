02e7a86ae303fd624656ff31b76f59d159409959
hwen <whman63@gmail.com>
2021-03-11 20:27:21 +0800

    322. Coin Change

---
+++ leetcode/322.py
@@ -0,0 +1,18 @@
+# 322. Coin Change
+# https://leetcode.com/problems/coin-change/
+
+class Solution:
+    def coinChange(self, coins, amount):
+        level = seen = {0}
+        number = 0
+        
+        while level:
+            if amount in level:
+                return number
+            
+            level = {a+c for a in level for c in coins if a+c <= amount} - seen
+            seen |= level
+            number += 1
+            
+        return -1
+        
