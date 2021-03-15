d4cd37ffd799da38e96484428f67f91f9400d361
hwen <whman63@gmail.com>
2021-03-15 19:01:35 +0800

    535. Encode and Decode TinyURL

---
+++ leetcode/535.py
@@ -0,0 +1,14 @@
+# 535. Encode and Decode TinyURL
+# https://leetcode.com/problems/encode-and-decode-tinyurl/
+
+class Codec:
+​
+    def __init__(self):
+        self.urls = []
+​
+    def encode(self, longUrl):
+        self.urls.append(longUrl)
+        return 'http://tinyurl.com/' + str(len(self.urls) - 1)
+​
+    def decode(self, shortUrl):
+        return self.urls[int(shortUrl.split('/')[-1])]
