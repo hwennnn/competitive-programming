801e2182afdeaf2d91afb75203ccc4f4f4a085e3
hwen <whman63@gmail.com>
2021-03-22 20:38:29 +0800

    966. Vowel Spellchecker

---
+++ leetcode/966.py
@@ -0,0 +1,43 @@
+# 966. Vowel Spellchecker
+# https://leetcode.com/problems/vowel-spellchecker/
+
+class Solution:
+    def spellchecker(self, wordlist: List[str], queries: List[str]) -> List[str]:
+        s1 = set(wordlist)
+        s2 = set()
+        mp = {}
+        
+        def mapped(word):
+            s = []
+            for w in word:
+                if w in "aeiouAEIOU":
+                    s.append("_")
+                else:
+                    s.append(w.lower())
+            return "".join(s)
+        
+        for i, w in enumerate(wordlist):
+            l = "".join([c.lower() for c in w])
+            s2.add(l)
+            
+            if l not in mp:
+                mp[l] = i
+                
+            m = mapped(w)
+            if m not in mp:
+                mp[m] = i
+​
+        res = []
+        for q in queries:
+            l = "".join([c.lower() for c in q])
+            h = mapped(q)
+            if q in s1:
+                res.append(q)
+            elif l in s2:
+                res.append(wordlist[mp[l]])
+            elif h in mp:
+                res.append(wordlist[mp[h]])
+            else:
+                res.append("")
+        
+        return res
