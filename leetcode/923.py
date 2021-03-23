734505edfc6651149e6eb5e36f72ed8260804c4f
hwen <whman63@gmail.com>
2021-03-23 21:01:52 +0800

    923. 3Sum With Multiplicity

---
+++ leetcode/923.py
@@ -0,0 +1,18 @@
+# 923. 3Sum With Multiplicity
+# https://leetcode.com/problems/3sum-with-multiplicity/
+
+class Solution:
+    def threeSumMulti(self, A: List[int], target: int) -> int:
+        mp = collections.defaultdict(int)
+        n = len(A)
+        res = 0
+        M = 10 ** 9 + 7
+        
+        for i in range(n):
+            res = (res + mp[target - A[i]]) % M
+            
+            for j in range(i):
+                t = A[i] + A[j]
+                mp[t] += 1
+        
+        return res % M
