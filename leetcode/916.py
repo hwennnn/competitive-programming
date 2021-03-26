8422ec63760822a98e2d710378b8d2573867738f
hwen <whman63@gmail.com>
2021-03-26 22:39:33 +0800

    916. Word Subsets

---
+++ leetcode/916.py
@@ -0,0 +1,12 @@
+# 916. Word Subsets
+# https://leetcode.com/problems/word-subsets/
+
+class Solution:
+    def wordSubsets(self, A, B):
+        count = collections.Counter()
+        
+        for b in B:
+            count |= collections.Counter(b)
+            
+        return [a for a in A if not count - Counter(a)]
+​
