ad77fe30798b97fa14f59707a77d9d601ff03c60
hwen <whman63@gmail.com>
2021-03-28 12:57:54 +0800

    1807. Evaluate the Bracket Pairs of a String

--- leetcode/Contest/Weekly/234/1807.py
+++ leetcode/Contest/Weekly/234/1807.py
@@ -1,33 +1,23 @@
 # 1807. Evaluate the Bracket Pairs of a String
-# https://leetcode.com/problems/evaluate-the-bracket-pairs-of-a-string/
+# https://leetcode.com/problems/evaluate-the-bracket-pairs-of-a-string
 
 class Solution:
     def evaluate(self, s: str, knowledge: List[List[str]]) -> str:
-        mp = collections.defaultdict(str)
-        
-        for k,v in knowledge:
-            mp[k] = v
-            
+        mp = {k:v for k,v in knowledge}
         res = []
-        opened = None
         key = ""
+        going = False
         
         for i,x in enumerate(s):
-            if x == "(": opened = i
-                
+            if x == "(":
+                going = True
             elif x == ")":
-                if key not in mp:
-                    res.append("?")
-                else:
-                    res.append(mp[key])
+                going = False
+                res.append(mp.get(key, "?"))
                 key = ""
-                opened = None
-                continue
-                
-            if opened == None:
-                res.append(x)
+            elif going:
+                key += x
             else:
-                if x != "(":
-                    key += x
+                res.append(x)
         
         return "".join(res)
