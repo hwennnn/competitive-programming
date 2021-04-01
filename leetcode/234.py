035c185c4b040fc6b5489a4593f1d19faa87d1e0
hwen <whman63@gmail.com>
2021-04-01 15:37:46 +0800

    234. Palindrome Linked List

---
+++ leetcode/234.py
@@ -0,0 +1,19 @@
+# 234. Palindrome Linked List
+# https://leetcode.com/problems/palindrome-linked-list/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+​
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        
+        lst = []
+        
+        while head:
+            lst.append(head.val)
+            head = head.next
+            
+        return lst == lst[::-1]
