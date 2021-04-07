47b8895922058819609327323449cc530c755b48
hwen <whman63@gmail.com>
2021-04-07 18:34:32 +0800

    1704. Determine if String Halves Are Alike

--- leetcode/1704.py
+++ leetcode/1704.py
@@ -1,9 +1,9 @@
 # 1704. Determine if String Halves Are Alike
-# https://leetcode.com/problems/determine-if-string-halves-are-alike/
+# https://leetcode.com/problems/determine-if-string-halves-are-alike
 
 class Solution:
     def halvesAreAlike(self, s: str) -> bool:
         n = len(s) // 2
         ss = set("aeiouAEIOU")
 ​
-        return sum(1 for c in s[:n] if c in ss) == sum(1 for c in s[n:] if c in ss)
+        return sum(c in ss for c in s[:n]) == sum(c in ss for c in s[n:])
