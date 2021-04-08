85c5ac0676a2b1d479647610001ed11ae0044389
hwen <whman63@gmail.com>
2021-04-08 15:56:36 +0800

    17. Letter Combinations of a Phone Number

--- leetcode/17.py
+++ leetcode/17.py
@@ -2,22 +2,22 @@
 # https://leetcode.com/problems/letter-combinations-of-a-phone-number/
 
 class Solution:
-    def letterCombinations(self, digits: str):
-        if not digits: return []
-        nums = ["0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
-        
-        deq = collections.deque([""])   
-        
-        for d in map(int, digits):
-            tmp = []
-            
-            while deq:
-                curr = deq.popleft()
-                for digit in nums[d]:
-                    tmp.append(curr+digit)
-            
-            for e in tmp:
-                deq.append(e)
-
-        
-        return list(deq)
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits: return []
+        nums = ["0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
+        
+        deq = collections.deque([""])   
+        
+        for d in map(int, digits):
+            tmp = []
+            
+            while deq:
+                curr = deq.popleft()
+                for digit in nums[d]:
+                    tmp.append(curr+digit)
+            
+            for e in tmp:
+                deq.append(e)
+​
+        
+        return list(deq)
