a31862eaf8ec51d28fbf92ae57c9707dcbe674b6
hwen <whman63@gmail.com>
2021-04-08 16:12:24 +0800

    17. Letter Combinations of a Phone Number

--- leetcode/17.py
+++ leetcode/17.py
@@ -1,5 +1,5 @@
 # 17. Letter Combinations of a Phone Number
-# https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+# https://leetcode.com/problems/letter-combinations-of-a-phone-number
 
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
@@ -21,3 +21,21 @@ class Solution:
 ​
         
         return list(deq)
+​
+    def letterCombinations2(self, digits: str) -> List[str]:
+        if not digits: return []
+        
+        dic = {"2":"abc", '3':"def", '4':"ghi", '5':"jkl", '6':"mno", '7':"pqrs", '8':"tuv", '9':"wxyz"}
+        res = []
+        
+        self.dfs(dic, digits, 0, "", res)
+        
+        return res
+    
+    def dfs(self, dic, digits, index, curr, res):
+        if len(curr) == len(digits):
+            res.append(curr)
+            return
+​
+        for c in dic[digits[index]]:
+            self.dfs(dic, digits, index+1, curr+c, res)
