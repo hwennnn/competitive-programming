42726a0023526c7eef11f6263ea557c894c25ec3
hwen <whman63@gmail.com>
2021-04-10 14:19:29 +0800

    1781. Sum of Beauty of All Substrings

--- leetcode/Contest/Biweekly/47/1781.py
+++ leetcode/Contest/Biweekly/47/1781.py
@@ -1,16 +1,16 @@
 # 1781. Sum of Beauty of All Substrings
-# https://leetcode.com/problems/sum-of-beauty-of-all-substrings/
+# https://leetcode.com/problems/sum-of-beauty-of-all-substrings
 
 class Solution:
     def beautySum(self, s: str) -> int:
-        res = 0
         n = len(s)
+        res = 0
         
         for i in range(n):
-            for j in range(i+1, n):
-                sub = s[i:j+1]
-                c = collections.Counter(sub)
-                if len(c) > 1:
-                    res += max(c.values()) - min(c.values())
+            mp = collections.defaultdict(int)
+            for j in range(i, n):
+                mp[s[j]] += 1
+                v = mp.values()
+                res += max(v) - min(v)
         
         return res
