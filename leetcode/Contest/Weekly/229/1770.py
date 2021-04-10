455d6ff80443126b54ef21a06eedbb86cbd16e47
hwen <whman63@gmail.com>
2021-04-10 21:37:11 +0800

    1770. Maximum Score from Performing Multiplication Operations

---
+++ leetcode/Contest/Weekly/229/1770.py
@@ -0,0 +1,15 @@
+# 1770. Maximum Score from Performing Multiplication Operations
+# https://leetcode.com/problems/maximum-score-from-performing-multiplication-operations/
+
+class Solution:
+    def maximumScore(self, nums: List[int], muls: List[int]) -> int:
+        n, m = len(nums), len(muls)
+        
+        @lru_cache(2000)
+        def dp(l, i):
+            if i == m: return 0
+            pickLeft = dp(l+1, i+1) + nums[l] * muls[i] # Pick the left side
+            pickRight = dp(l, i+1) + nums[n-(i-l)-1] * muls[i] # Pick the right side
+            return max(pickLeft, pickRight)
+        
+        return dp(0, 0)
