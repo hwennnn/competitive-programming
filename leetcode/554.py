632368f680a25b12ceb96cb98fe98c5101c2e5de
hwen <whman63@gmail.com>
2021-04-22 20:43:26 +0800

    554. Brick Wall

---
+++ leetcode/554.py
@@ -0,0 +1,17 @@
+# 554. Brick Wall
+# https://leetcode.com/problems/brick-wall/
+
+class Solution:
+    def leastBricks(self, wall: List[List[int]]) -> int:
+        n = len(wall)
+        
+        mp = collections.defaultdict(int)
+        
+        for bricks in wall:
+            start = 0
+            for i in range(len(bricks) - 1):
+                start += bricks[i]
+                mp[start] += 1
+        
+        return n - (max(mp.values()) if len(mp) > 0 else 0)
+            
