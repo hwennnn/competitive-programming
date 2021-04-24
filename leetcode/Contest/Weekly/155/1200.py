a64fb946d70f220f2951964b5a3e118432e3f262
hwen <whman63@gmail.com>
2021-04-25 01:41:25 +0800

    1200. Minimum Absolute Difference

---
+++ leetcode/Contest/Weekly/155/1200.py
@@ -0,0 +1,9 @@
+# 1200. Minimum Absolute Difference
+# https://leetcode.com/problems/minimum-absolute-difference/
+
+class Solution:
+    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+        arr.sort()
+        m = min(b - a for a,b in zip(arr, arr[1:]))
+        return [[a,b] for a,b in zip(arr, arr[1:]) if b - a == m]
+        
