cb55005d047848596f8baf1da75afa83614a0e69
hwen <whman63@gmail.com>
2021-04-30 23:27:39 +0800

    970. Powerful Integers

---
+++ leetcode/970.py
@@ -0,0 +1,19 @@
+# 970. Powerful Integers
+# https://leetcode.com/problems/powerful-integers/
+
+class Solution:
+    def powerfulIntegers(self, x: int, y: int, bound: int) -> List[int]:
+        res = set()
+        
+        i = 1
+        while i < bound:
+            j = 1
+            while i + j <= bound:
+                res.add(i + j)
+                if y == 1: break
+                j *= y
+            
+            if x == 1: break
+            i *= x
+        
+        return list(res)
