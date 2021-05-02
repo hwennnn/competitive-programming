c696d079c1902fb4e31dd66a9970ed85391fa404
hwen <whman63@gmail.com>
2021-05-02 23:07:42 +0800

    630. Course Schedule III

---
+++ leetcode/630.py
@@ -0,0 +1,16 @@
+# 630. Course Schedule III
+# https://leetcode.com/problems/course-schedule-iii/
+
+class Solution:
+    def scheduleCourse(self, A: List[List[int]]) -> int:
+        A.sort(key = lambda x:x[1])
+        pq = []
+        curr = 0
+        
+        for i, (t,l) in enumerate(A):
+            curr += t
+            heapq.heappush(pq, -t)
+            if curr > l:
+                curr += heapq.heappop(pq)
+            
+        return len(pq)
