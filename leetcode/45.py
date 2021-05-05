bdfe5f02dfad0833c998ce839de20cd358923e2f
hwen <whman63@gmail.com>
2021-05-05 20:31:50 +0800

    45. Jump Game II

---
+++ leetcode/45.py
@@ -0,0 +1,16 @@
+# 45. Jump Game II
+# https://leetcode.com/problems/jump-game-ii/
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        if len(nums) == 1: return 0
+        
+        left, right = 0, nums[0]
+        res = 1
+        
+        while right < len(nums) - 1:
+            res += 1
+            nxt = max(i + nums[i] for i in range(left, right + 1))
+            left, right = right, nxt
+        
+        return res
