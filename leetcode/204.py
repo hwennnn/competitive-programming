4197b30f0fd96f3ff8f91825dbc73811eda0434f
hwen <whman63@gmail.com>
2021-05-10 17:37:19 +0800

    204. Count Primes

---
+++ leetcode/204.py
@@ -0,0 +1,16 @@
+# 204. Count Primes
+# https://leetcode.com/problems/count-primes/
+
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n <= 1: return 0
+        
+        arr = [True] * n
+        arr[0] = arr[1] = False
+        
+        for i in range(2, n):
+            if arr[i]:
+                for j in range(i * i, n, i):
+                    arr[j] = False
+        
+        return sum(arr)
