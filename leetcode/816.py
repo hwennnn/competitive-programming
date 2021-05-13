2c930d76f693673ee22cf65d573ba748631bb41e
hwen <whman63@gmail.com>
2021-05-13 21:23:21 +0800

    816. Ambiguous Coordinates

--- leetcode/816.py
+++ leetcode/816.py
@@ -2,20 +2,24 @@
 # https://leetcode.com/problems/ambiguous-coordinates/
 
 class Solution:
-    def ambiguousCoordinates(self, S):
+    def ambiguousCoordinates(self, s: str) -> List[str]:
         
         def generate(s):
-            ans = []
-            if s == "0" or s[0] != "0": ans.append(s)
+            res = []
+            
+            if s == "0" or s[0] != "0": res.append(s)
+            
             for i in range(1, len(s)):
-                if (s[:i] == "0" or s[0] != "0") and s[-1] != "0":
-                    ans.append(s[:i] + "." + s[i:])
-            return ans
+                if (s[:i] == "0" or s[:i][0] != "0") and s[i:][-1] != "0":
+                    res.append(s[:i] + "." + s[i:])
+            
+            return res
         
-        n, ans = len(S), []
-        for i in range(2, n-1):
-            cand_l, cand_r = generate(S[1:i]), generate(S[i:-1])   
-            for l, r in product(cand_l, cand_r):
-                ans.append("(" + l + ", " + r + ")")
-                
-        return ans
+        res = []
+        for i in range(2, len(s) - 1):
+            left, right = generate(s[1:i]), generate(s[i:-1])
+​
+            for l, r in product(left, right):
+                res.append("(" + l + ", " + r + ")")
+        
+        return res
