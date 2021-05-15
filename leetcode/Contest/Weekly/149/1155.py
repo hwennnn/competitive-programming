7399d4b0af5a15451b11184cbafd916a021af1d3
hwen <whman63@gmail.com>
2021-05-15 21:36:53 +0800

    1155. Number of Dice Rolls With Target Sum

---
+++ leetcode/Contest/Weekly/149/1155.py
@@ -0,0 +1,18 @@
+# 1155. Number of Dice Rolls With Target Sum
+# https://leetcode.com/problems/number-of-dice-rolls-with-target-sum/
+
+class Solution:
+    def numRollsToTarget(self, d: int, f: int, target: int) -> int:
+        M = 10 ** 9 + 7
+        
+        @cache
+        def dp(d, target):
+            if d == 0: return 1 if target == 0 else 0
+            
+            count = 0
+            for k in range(max(0, target - f), target):
+                count += dp(d - 1, k)
+            
+            return count
+            
+        return dp(d, target) % M
