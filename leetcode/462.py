e80e31b404aa5370eabf6b35c02e24e24f56145b
hwen <whman63@gmail.com>
2021-05-19 19:39:51 +0800

    462. Minimum Moves to Equal Array Elements II

--- leetcode/462.py
+++ leetcode/462.py
@@ -1,11 +1,8 @@
 # 462. Minimum Moves to Equal Array Elements II
-# https://leetcode.com/problems/minimum-moves-to-equal-array-elements-ii/
+# https://leetcode.com/problems/minimum-moves-to-equal-array-elements-ii
 
 class Solution:
     def minMoves2(self, nums: List[int]) -> int:
         nums.sort()
-        n = len(nums)
         
-        median = nums[n // 2] if n % 2 else (nums[n // 2] + nums[n // 2 - 1]) // 2
-        
-        return sum(abs(x - median) for x in nums)
+        return sum(abs(x - nums[len(nums) // 2]) for x in nums)
