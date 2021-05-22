35af8e5faf2defa732693be4405db7c7d027fe08
hwen <whman63@gmail.com>
2021-05-22 20:53:55 +0800

    1147. Longest Chunked Palindrome Decomposition

---
+++ leetcode/Contest/Weekly/148/1147-2.py
@@ -0,0 +1,12 @@
+# 1147. Longest Chunked Palindrome Decomposition
+# https://leetcode.com/problems/longest-chunked-palindrome-decomposition/
+
+class Solution:
+    def longestDecomposition(self, text: str) -> int:
+        n = len(text)
+        
+        for i in range(n // 2):
+            if text[:i + 1] == text[n - i - 1:]:
+                return 2 + self.longestDecomposition(text[i + 1: n - i - 1])
+        
+        return 0 if n == 0 else 1
