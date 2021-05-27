a399d8bede0c1532e35427864d9d887f0582e1d8
hwen <whman63@gmail.com>
2021-05-27 15:47:44 +0800

    318. Maximum Product of Word Lengths

---
+++ leetcode/318-2.py
@@ -0,0 +1,17 @@
+# 318. Maximum Product of Word Lengths
+# https://leetcode.com/problems/maximum-product-of-word-lengths
+
+class Solution:
+    def maxProduct(self, words: List[str]) -> int:
+        n = len(words)
+        mp = collections.defaultdict(int)
+        
+        for word in words:
+            mask = 0
+            
+            for c in word:
+                mask |= 1 << (ord(c) - ord('a'))
+            
+            mp[mask] = max(mp[mask], len(word))
+        
+        return max([mp[mask1] * mp[mask2] for mask1 in mp for mask2 in mp if mask1 & mask2 == 0] or [0])
