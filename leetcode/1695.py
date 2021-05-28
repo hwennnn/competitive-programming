6f6ed80bafd64febd415e6f3cde366c43495df1d
hwen <whman63@gmail.com>
2021-05-28 17:11:33 +0800

    1695. Maximum Erasure Value

---
+++ leetcode/1695.py
@@ -0,0 +1,23 @@
+# 1695. Maximum Erasure Value
+# https://leetcode.com/problems/maximum-erasure-value/
+
+class Solution:
+    def maximumUniqueSubarray(self, nums: List[int]) -> int:
+        
+        c = collections.Counter()
+        curr = res = i = 0
+        
+        for j,x in enumerate(nums):
+            curr += x
+            c[x] += 1
+            
+            while c[x] >= 2:
+                target = nums[i]
+                c[target] -= 1
+                curr -= target
+                i += 1
+            
+            res = max(res, curr)
+        
+        return res
+            
