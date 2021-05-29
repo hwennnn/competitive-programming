2123b8816ea16607ad27fdbfbb8164819e209b49
hwen <whman63@gmail.com>
2021-05-29 22:16:02 +0800

    1081. Smallest Subsequence of Distinct Characters

---
+++ leetcode/Contest/Weekly/140/1081.py
@@ -0,0 +1,18 @@
+# 1081. Smallest Subsequence of Distinct Characters
+# https://leetcode.com/problems/smallest-subsequence-of-distinct-characters/
+
+class Solution:
+    def smallestSubsequence(self, s: str) -> str:
+        counter, visited, stack = collections.Counter(s), set(), []
+        
+        for c in s:
+            counter[c] -= 1
+            if c in visited: continue
+            
+            while stack and stack[-1] > c and counter[stack[-1]] > 0:
+                visited.remove(stack.pop())
+            
+            visited.add(c)
+            stack.append(c)
+        
+        return "".join(stack)
