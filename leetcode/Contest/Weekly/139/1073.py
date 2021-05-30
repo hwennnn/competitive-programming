f07ed8413e952fcb9bb86db54fc0347adbb38dae
hwen <whman63@gmail.com>
2021-05-30 21:40:48 +0800

    1073. Adding Two Negabinary Numbers

---
+++ leetcode/Contest/Weekly/139/1073.py
@@ -0,0 +1,16 @@
+# 1073. Adding Two Negabinary Numbers
+# https://leetcode.com/problems/adding-two-negabinary-numbers/
+
+class Solution:
+    def addNegabinary(self, A: List[int], B: List[int]) -> List[int]:
+        res = []
+        carry = 0
+        
+        while A or B or carry:
+            carry += (A or [0]).pop() + (B or [0]).pop()
+            res.append(carry & 1)
+            carry = -(carry >> 1)
+        
+        while len(res) > 1 and res[-1] == 0: res.pop()
+        
+        return res[::-1]
