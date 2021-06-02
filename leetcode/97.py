7f274efe01bab1a5608fb64223ab047216d139ca
hwen <whman63@gmail.com>
2021-06-02 18:49:46 +0800

    97. Interleaving String

--- leetcode/97.py
+++ leetcode/97.py
@@ -1,23 +1,25 @@
 # 97. Interleaving String
-# https://leetcode.com/problems/interleaving-string/
+# https://leetcode.com/problems/interleaving-string
 
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         n1, n2, n3 = len(s1), len(s2), len(s3)
-        queue = collections.deque([(0, 0, 0)])
-        visited = set()
+        if n1 + n2 != n3: return False
+        
+        queue = collections.deque([(0, 0)])
+        visited = set([(0,0)])
         
         while queue:
-            p1, p2, p3 = queue.popleft()
-            if p1 == n1 and p2 == n2 and p3 == n3: return True
+            p1, p2 = queue.popleft()
             
-            if p3 < n3:
-                if p1 < n1 and s1[p1] == s3[p3] and (p1 + 1, p2, p3 + 1) not in visited:
-                    queue.append((p1 + 1, p2, p3 + 1))
-                    visited.add((p1 + 1, p2, p3 + 1))
+            if p1 + p2 == n3: return True
+            
+            if p1 < n1 and s1[p1] == s3[p1 + p2] and (p1 + 1, p2) not in visited:
+                queue.append((p1 + 1, p2))
+                visited.add((p1 + 1, p2))
 ​
-                if p2 < n2 and s2[p2] == s3[p3] and (p1, p2 + 1, p3 + 1) not in visited:
-                    queue.append((p1, p2 + 1, p3 + 1))
-                    visited.add((p1, p2 + 1, p3 + 1))
-        
+            if p2 < n2 and s2[p2] == s3[p1 + p2] and (p1, p2 + 1) not in visited:
+                queue.append((p1, p2 + 1))
+                visited.add((p1, p2 + 1))
+​
         return False
