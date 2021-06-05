d0db499dfbe709e976495dcdcf253b36fb6be5c9
hwen <whman63@gmail.com>
2021-06-05 22:43:40 +0800

    1046. Last Stone Weight

---
+++ leetcode/Contest/Weekly/137/1046.py
@@ -0,0 +1,14 @@
+# 1046. Last Stone Weight
+# https://leetcode.com/problems/last-stone-weight/
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        
+        while len(stones) > 1:
+            stones.sort()
+            if stones[-1] == stones[-2]:
+                stones.pop(); stones.pop()
+            else:
+                stones.append(stones.pop() - stones.pop())
+        
+        return stones[0] if stones else 0
