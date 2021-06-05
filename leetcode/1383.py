12e4d4030147b583ce9ef843cb6f8b64a2998705
hwen <whman63@gmail.com>
2021-06-05 15:41:24 +0800

    1383. Maximum Performance of a Team

---
+++ leetcode/1383.py
@@ -0,0 +1,18 @@
+# 1383. Maximum Performance of a Team
+# https://leetcode.com/problems/maximum-performance-of-a-team/
+
+class Solution:
+    def maxPerformance(self, n: int, speed: List[int], efficiency: List[int], k: int) -> int:
+        heap = []
+        res = ssum = 0
+        
+        for e, s in sorted(zip(efficiency, speed), reverse = True):
+            heapq.heappush(heap, s)    
+            ssum += s
+            
+            if len(heap) > k:
+                ssum -= heapq.heappop(heap)
+            
+            res = max(res, e * ssum)
+        
+        return res % (10 ** 9 + 7)
