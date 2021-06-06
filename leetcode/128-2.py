d400fb4b978b9cab0d03d98e3d464e5ff5e54842
hwen <whman63@gmail.com>
2021-06-06 18:47:45 +0800

    128. Longest Consecutive Sequence

---
+++ leetcode/128-2.py
@@ -0,0 +1,22 @@
+# 128. Longest Consecutive Sequence
+# https://leetcode.com/problems/longest-consecutive-sequence
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        s = set(nums)
+        res = 0
+        
+        while s:
+            first = last = s.pop()
+            
+            while first - 1 in s:
+                s.remove(first - 1)
+                first -= 1
+            
+            while last + 1 in s:
+                s.remove(last + 1)
+                last += 1
+            
+            res = max(res, last - first + 1)
+        
+        return res
