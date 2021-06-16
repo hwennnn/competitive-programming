bb5e09b91cfe2284607e7db904eccadf62a07068
hwen <whman63@gmail.com>
2021-06-16 22:05:11 +0800

    22. Generate Parentheses

---
+++ leetcode/22-2.py
@@ -0,0 +1,21 @@
+# 22. Generate Parentheses
+# https://leetcode.com/problems/generate-parentheses
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        
+        def backtrack(curr = "", opened = 0, closed = 0):
+            if opened == closed == n:
+                res.append(curr)
+                return
+            
+            if closed < opened:
+                backtrack(curr + ")", opened, closed + 1)
+            
+            if opened < n:
+                backtrack(curr + "(", opened + 1, closed)
+​
+        backtrack()
+        
+        return res
