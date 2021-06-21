a2da9850e40e313648b6ca666a9f682ba5982fa8
hwen <whman63@gmail.com>
2021-06-21 15:30:05 +0800

    118. Pascal's Triangle

---
+++ leetcode/118.py
@@ -0,0 +1,17 @@
+# 118. Pascal's Triangle
+# https://leetcode.com/problems/pascals-triangle/
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = [[0] * (i + 1) for i in range(numRows)]
+        
+        for i, row in enumerate(res):
+            row[0] = row[-1] = 1
+            
+            for j in range(1, len(row) - 1):
+                left = res[i - 1][j - 1]
+                right = res[i - 1][j]
+                
+                row[j] = left + right
+            
+        return res
