aadd1a5511e362ba7db73f3a54fa69853fd26cf8
hwen <whman63@gmail.com>
2021-06-22 17:58:59 +0800

    792. Number of Matching Subsequences

---
+++ leetcode/792.py
@@ -0,0 +1,23 @@
+# 792. Number of Matching Subsequences
+# https://leetcode.com/problems/number-of-matching-subsequences
+
+class Solution:
+    def numMatchingSubseq(self, s: str, words: List[str]) -> int:
+        mp = collections.defaultdict(collections.deque)
+        
+        for w in words:
+            mp[w[0]].append(w)
+        
+        res = 0
+        
+        for c in s:
+            n = len(mp[c])
+            
+            for _ in range(n):
+                word = mp[c].popleft()
+                if len(word) == 1:
+                    res += 1
+                else:
+                    mp[word[1]].append(word[1:])
+        
+        return res
