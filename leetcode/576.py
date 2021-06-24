f7b83bc1ed8fef5aac9f42eed3ccd593ef7b1dee
hwen <whman63@gmail.com>
2021-06-24 21:07:22 +0800

    576. Out of Boundary Paths

---
+++ leetcode/576.py
@@ -0,0 +1,20 @@
+# 576. Out of Boundary Paths
+# https://leetcode.com/problems/out-of-boundary-paths
+
+class Solution:
+    def findPaths(self, rows: int, cols: int, maxMove: int, startRow: int, startColumn: int) -> int:
+        
+        @cache
+        def go(x, y, moves):
+            if not (0 <= x < rows and 0 <= y < cols): return 1
+            
+            if moves == maxMove: return 0
+            
+            res = 0
+            
+            for dx, dy in ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)):
+                res += go(dx, dy, moves + 1)
+            
+            return res
+        
+        return go(startRow, startColumn, 0) % (10 ** 9 + 7)
