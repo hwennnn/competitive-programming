4270e4831c4bfcea8e8ac10a4188b6af6383c607
hwen <whman63@gmail.com>
2021-06-27 00:18:39 +0800

    1910. Remove All Occurrences of a Substring

---
+++ leetcode/Contest/Biweekly/55/1910.py
@@ -0,0 +1,10 @@
+# 1910. Remove All Occurrences of a Substring
+# https://leetcode.com/problems/remove-all-occurrences-of-a-substring/
+
+class Solution:
+    def removeOccurrences(self, s: str, part: str) -> str:
+        
+        while part in s:
+            s = s.replace(part, '')
+            
+        return s
