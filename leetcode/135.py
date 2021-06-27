6bc980dc4469518f301ddc8c330be6da4ab862d7
hwen <whman63@gmail.com>
2021-06-27 15:50:36 +0800

    135. Candy

---
+++ leetcode/135.py
@@ -0,0 +1,20 @@
+# 135. Candy
+# https://leetcode.com/problems/candy/
+
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        n = len(ratings)
+        res = [-1] * n
+​
+        for _, index in sorted((rating, index) for index,rating in enumerate(ratings)):
+            current = 1
+            
+            if index - 1 >= 0 and ratings[index] > ratings[index - 1]:
+                current = max(current, res[index - 1] + 1)
+            
+            if index + 1 < n and ratings[index] > ratings[index + 1]:
+                current = max(current, res[index + 1] + 1)
+            
+            res[index] = current
+        
+        return sum(res)
