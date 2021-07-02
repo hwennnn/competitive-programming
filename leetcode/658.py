9dab687aa09d7236f8aba7a620b42b9cf9fb649b
hwen <whman63@gmail.com>
2021-07-02 16:45:34 +0800

    658. Find K Closest Elements

---
+++ leetcode/658.py
@@ -0,0 +1,21 @@
+# 658. Find K Closest Elements
+# https://leetcode.com/problems/find-k-closest-elements
+
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        if len(arr) == k: return arr
+        
+        i = bisect.bisect_left(arr, x) - 1
+        j = i + 1
+        
+        while j - i - 1 < k:
+            if i == -1:
+                j += 1
+                continue
+            
+            if j == len(arr) or abs(arr[i] - x) <= abs(arr[j] - x):
+                i -= 1
+            else:
+                j += 1
+            
+        return arr[i + 1: j]
