21a631abae02252e7bce815f9486a11c006bafac
hwen <whman63@gmail.com>
2021-07-04 12:38:23 +0800

    1920. Build Array from Permutation

---
+++ leetcode/Contest/Weekly/248/1920.py
@@ -0,0 +1,11 @@
+# 1920. Build Array from Permutation
+# https://leetcode.com/problems/build-array-from-permutation/
+
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        res = [0] * len(nums)
+        
+        for i in range(len(nums)):
+            res[i] = nums[nums[i]]
+        
+        return res
