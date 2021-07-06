1141448feb9640824c031ab001f74ffbc305395b
hwen <whman63@gmail.com>
2021-07-06 19:56:37 +0800

    50. Pow(x, n)

---
+++ leetcode/50.py
@@ -0,0 +1,12 @@
+# 50. Pow(x, n)
+# https://leetcode.com/problems/powx-n
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0: return 1
+        
+        if n < 0:
+            n = -n
+            x = 1 / x
+        
+        return self.myPow(x * x, n // 2) if n % 2 == 0 else x * self.myPow(x * x, n // 2)
