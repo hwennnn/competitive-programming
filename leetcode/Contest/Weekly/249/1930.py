f00d4a5f91b919b6649051586d9cd8c4ff257eea
hwen <whman63@gmail.com>
2021-07-11 12:03:30 +0800

    1930. Unique Length-3 Palindromic Subsequences

---
+++ leetcode/Contest/Weekly/249/1930.py
@@ -0,0 +1,20 @@
+# 1930. Unique Length-3 Palindromic Subsequences
+# https://leetcode.com/problems/unique-length-3-palindromic-subsequences/
+
+class Solution:
+    def countPalindromicSubsequence(self, s: str) -> int:
+        res = 0
+        n = len(s)
+        mp = collections.defaultdict(list)
+        
+        for i,x in enumerate(s):
+            mp[x].append(i)
+        
+        for key in mp:
+            ll = mp[key]
+            if len(ll) >= 2:
+                ss = set(s[ll[0] + 1 : ll[-1]])     
+                res += len(ss)
+​
+        return res
+        
