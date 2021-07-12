ded120d0e900e2362599de5ed9143d3edd8e84cc
hwen <whman63@gmail.com>
2021-07-12 17:45:33 +0800

    205. Isomorphic Strings

---
+++ leetcode/205.py
@@ -0,0 +1,20 @@
+# 205. Isomorphic Strings
+# https://leetcode.com/problems/isomorphic-strings
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        s, t = list(s), list(t)
+        s1, s2 = {}, {}
+        
+        for i, (a,b) in enumerate(zip(s, t)):
+            if a in s1:
+                s[i] = s1[a]
+            else:
+                s[i] = s1[a] = len(s1)
+            
+            if b in s2:
+                t[i] = s2[b]
+            else:
+                t[i] = s2[b] = len(s2)
+        
+        return s == t
